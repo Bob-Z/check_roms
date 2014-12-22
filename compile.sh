d9132468c3b1653a025b118ef4d45499444b37f6
Bob-Z <carabobz@gmail.com>
2014-12-22 20:50:49 +0100

    Name output file

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gcc -Wall -g -O0 -D_FILE_OFFSET_BITS=64 main.c parse_data.c linked_list.c -lexpat -pthread
-#gcc -g -O0 main.c parse_data.c linked_list.c `xml2-config --cflags` `xml2-config --libs`
+gcc -Wall -g -O0 -D_FILE_OFFSET_BITS=64 main.c parse_data.c linked_list.c -lexpat -pthread -o check_roms
